02467b0bc97dc8f4c31c7b20772bd2bd30a5118c
KarakenHunter <sharmamanthan474@gmail.com>
2026-03-22 00:20:16 +0530

    feat:add continuous connection acceptance loop

--- Socket/Multi-Client RCE/run.py
+++ Socket/Multi-Client RCE/run.py
@@ -7,3 +7,12 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('localhost', 9821))
 print("[*] Waiting for connection...")
 sock.listen()
+collection = {}
+
+
+while True:
+    client, addr = sock.accept()
+    collection[f'{addr}'] = f'{client}'
+    sys.stdout.write(f"\rConnected clients:\n {collection}")
+    sys.stdout.flush()
+
